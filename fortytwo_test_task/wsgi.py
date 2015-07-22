8a18bfb8ca345c4225a94c937f1dce22c5df1a65
Sergii Vanzha <terkel919@gmail.com>
2015-07-22 16:51:57 +0300

    ticket:1 move up django.setup()

--- fortytwo_test_task/wsgi.py
+++ fortytwo_test_task/wsgi.py
@@ -6,12 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fortytwo_test_task.settings")
 import django
 
 django.setup()
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fortytwo_test_task.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
