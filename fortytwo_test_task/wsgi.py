af076624323c0500ddd33fdad796d8b258997e50
Sergii Vanzha <terkel919@gmail.com>
2015-07-22 14:38:10 +0300

    ticket:1 back to django.setup()

--- fortytwo_test_task/wsgi.py
+++ fortytwo_test_task/wsgi.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fortytwo_test_task.settings")
+import django
 
+django.setup()
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
