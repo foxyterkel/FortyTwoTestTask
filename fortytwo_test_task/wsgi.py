5c672db42b0ab8370653d1a98a500b774a3556b0
Sergii Vanzha <terkel919@gmail.com>
2015-07-22 13:20:05 +0300

    ticket:1 delete django.setup()

--- fortytwo_test_task/wsgi.py
+++ fortytwo_test_task/wsgi.py
@@ -9,9 +9,6 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fortytwo_test_task.settings")
-import django
-
-django.setup()
 
 
 from django.core.wsgi import get_wsgi_application
