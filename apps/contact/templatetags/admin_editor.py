e409cf834dcfdba307be73f44b8e8352aad0146b
Sergii Vanzha <terkel919@gmail.com>
2015-07-27 17:27:28 +0300

    ticket:8 editor done

--- apps/contact/templatetags/admin_editor.py
+++ apps/contact/templatetags/admin_editor.py
@@ -4,9 +4,9 @@ from django.core.urlresolvers import reverse
 register = template.Library()
 
 
-# @register.simple_tag()
-# def admin_editor_url(obj):
-#     url = reverse('admin:%s_%s_change' % (obj._meta.app_label,
-#                   obj._meta.model_name), args=[obj.pk])
-#     return ('<a id="admin_edit" class="btn btn-default'
-#             'col-md-1" href=%s>Admin edit</a>' % (url))
+@register.simple_tag()
+def admin_editor_url(obj):
+    url = reverse('admin:%s_%s_change' % (obj._meta.app_label,
+                  obj._meta.model_name), args=[obj.pk])
+    return ('<a id="admin_edit" class="btn btn-default'
+            'col-md-1" href=%s>Admin edit</a>' % (url))
