89978613f4b9523d17875b0eb73d15fc870eefcf
Sergii Vanzha <terkel919@gmail.com>
2015-07-27 15:52:26 +0300

    ticket:3 done

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -53,7 +53,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'apps.contact.MyMiddleWare.MyMiddleWare',
+    'apps.contact.MyMiddleWare.MyMiddleWare',
 
 )
 
